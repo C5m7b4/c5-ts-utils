cbd8de4f0fb9cf13c83afa38d97ea5c513f0600e
C5m7b4 <mbedingfield@dcrpos.com>
2022-02-18 14:27:00 -0600

    add filter capability

--- src/components/grid/Columns.tsx
+++ src/components/grid/Columns.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { createPortal } from 'react-dom';
+import { Filter } from './Filter';
 
 import './Columns.css';
+import { TableHeader } from '../DataGrid';
 
 export interface ColumnProps<T> {
   open: boolean;
@@ -10,11 +12,62 @@ export interface ColumnProps<T> {
   headers: string[];
   checkedColumns: string[];
   handleCheckClick: (e: string, checked: boolean) => void;
+  header: TableHeader<T>;
+  identifier: string;
+  data: T[];
+  filterChecked: boolean;
+  checkedFilters: string[];
+  setCheckedFilters: (s: string[]) => void;
+  handleFilterItemClick: (checked: boolean, r: string, header: string) => void;
+  handleMainFilterClick: (checked: boolean, s: string) => void;
+  showFilter: boolean;
+  setShowFilter: (b: boolean) => void;
 }
 export function Columns<T>(props: ColumnProps<T>) {
-  const { open, divId, style, headers, checkedColumns, handleCheckClick } =
-    props;
+  const [filterStyle, setFilterStyle] = useState<React.CSSProperties>({
+    position: 'absolute',
+  });
+
+  const {
+    open,
+    divId,
+    style,
+    headers,
+    checkedColumns,
+    handleCheckClick,
+    header,
+    filterChecked,
+    handleFilterItemClick,
+    checkedFilters,
+    handleMainFilterClick,
+    showFilter,
+    setShowFilter,
+  } = props;
   const div = document.getElementById(divId);
+
+  const handleMouseOver = () => {
+    const div = document.getElementById(
+      `mikto-table-filter-${props.identifier}`
+    );
+    if (div) {
+      const rect = div.getBoundingClientRect();
+      if (rect) {
+        setFilterStyle({
+          position: 'fixed',
+          zIndex: '5',
+          left: rect.right.toString() + 'px',
+          top: (rect.top - 200).toString() + 'px',
+          backgroundColor: '#aaa',
+          padding: '8px',
+        });
+        setShowFilter(true);
+      }
+    }
+  };
+
+  const handleMouseLeave = () => {
+    //setShowFilter(false);
+  };
 
   const amIChecked = (title: keyof T): boolean => {
     return checkedColumns.includes(title as string);
@@ -51,6 +104,31 @@ export function Columns<T>(props: ColumnProps<T>) {
               </div>
             );
           })}
+          {header.filterable ? (
+            <div onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+              <h3>Filter</h3>
+              <input
+                type="checkbox"
+                checked={filterChecked}
+                onChange={(e) =>
+                  handleMainFilterClick(
+                    e.target.checked,
+                    header.columnName as string
+                  )
+                }
+              />
+              <div id={`mikto-table-filter-${props.identifier}`}></div>
+              <Filter
+                header={header}
+                open={showFilter}
+                divId={`mikto-table-filter-${props.identifier}`}
+                data={props.data}
+                style={filterStyle}
+                availableFilters={checkedFilters}
+                filterItemClicked={handleFilterItemClick}
+              />
+            </div>
+          ) : null}
         </div>
       </div>,
       div
